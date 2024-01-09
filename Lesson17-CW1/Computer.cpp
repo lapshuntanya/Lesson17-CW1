ed366daa0b163461778960878cd5575bc908be90
Lapshun Tetiana <lapshun.tatiana@gmail.com>
2024-01-09 17:52:32 +0200

    definition methods

--- Lesson17-CW1/Computer.cpp
+++ Lesson17-CW1/Computer.cpp
@@ -1,1 +1,24 @@
 #include "Computer.h"
+
+Computer::Computer(string name, float frequency, int OZU, bool DVD, float cost)
+{
+	this->name = name;
+	this->frequency = frequency;
+	this->OZU = OZU;
+	this->DVD = DVD;
+	this->cost = cost;	
+}
+
+int Computer::getOZU() const
+{
+	return OZU;
+}
+
+void Computer::show() const
+{
+	cout << "Name: " << name << endl;
+	cout << "Frequency: " << frequency << "GHz" << endl;
+	cout << "OZU: " << OZU << "Gb" << endl;
+	cout << "DVD: " << DVD << endl;
+	cout << "Cost: " << cost << "UAH" << endl;
+}
